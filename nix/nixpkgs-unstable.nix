45a8a945b52f706d327bb8971e67f7f34a4b3444
Kirill Valyavin <k.valyavin@tinkoff.ru>
2021-04-16 18:13:10 +0300

    bump nixpkgs

--- nix/nixpkgs-unstable.nix
+++ nix/nixpkgs-unstable.nix
@@ -2,7 +2,7 @@
   {
     owner  = "NixOS";
     repo   = "nixpkgs";
-    # 2021-03-21
-    rev    = "4e0d3868c679da20108db402785f924daa1a7fb5";
+    # 2021-04-16
+    rev    = "5268ee2ebacbc73875be42d71e60c2b5c1b5a1c7";
     sha256 = "17ypsp6fmyywck1ad6fn77msg2xjlkgwv9ipzb4nw9hpca40hlss";
   }
